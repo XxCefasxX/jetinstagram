b87b62989e5d03f992c1fa0f0ddea4f4866a0371
XxCefasxX <salazar9_5@hotmail.com>
2023-02-22 21:57:59 -0500

    implemented repsitory an blocko un profile

--- app/src/main/java/com/vipulasri/jetinstagram/ui/profile/Profile.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/ui/profile/Profile.kt
@@ -6,26 +6,27 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.Switch
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import com.vipulasri.jetinstagram.R
+import com.vipulasri.jetinstagram.data.FriendsRepository
 import com.vipulasri.jetinstagram.data.StoriesRepository
 import com.vipulasri.jetinstagram.model.User
 import com.vipulasri.jetinstagram.model.currentUser
@@ -37,6 +38,10 @@ fun ProfileBody() {
 
     Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         // add here each component of body
+        ProfileHeader()
+        Divider()
+        PostBlock("https://source.unsplash.com/random/400x300?1")
+        Divider()
         SingleProfileOption("Tag People")
         Divider()
         SingleProfileOption("Add Location")
@@ -44,7 +49,7 @@ fun ProfileBody() {
         SingleProfileOption("Add Fundraiser")
         Divider()
         //implement new repository for friends accounts
-        val friends by StoriesRepository.observeStories()
+        val friends by FriendsRepository.observeFriends()
         for (friend in friends) {
             ToggleProfileOption(textOption = friend.name, image = friend.image)
         }
@@ -121,6 +126,73 @@ fun AccountImage(imageUrl: String) {
                 modifier = Modifier.fillMaxSize()
             )
         }
+    }
+}
+
+@Composable
+fun ProfileHeader() {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp)
+            .padding(5.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Icon(
+            ImageBitmap.imageResource(id = R.drawable.ic_back),
+            "",
+            modifier = Modifier
+                .size(40.dp)
+        )
+        Text(
+            text = "New Post", modifier = Modifier.weight(1f), fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Share",
+            fontWeight = FontWeight.Bold,
+            color = Color(15, 203, 255)
+        )
+    }
+}
+
+
+@Composable
+fun PostBlock(postImage: String) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(140.dp)
+            .padding(5.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
+    ) {
+        Image(
+            painter = rememberImagePainter(postImage),
+            contentDescription = null,
+            modifier = Modifier.height(100.dp)
+        )
+
+
+
+        val inputvalue = remember { mutableStateOf(TextFieldValue()) }
+
+        TextField(
+            modifier = Modifier
+                .height(100.dp)
+                .weight(1f)
+                .background(Color.White),
+            value = inputvalue.value,
+            singleLine = false,
+            onValueChange = {
+                inputvalue.value = it
+            },
+            placeholder = {
+                Text(text = "Write a caption", color = Color.LightGray)
+            }
+        )
     }
 }
 
